9283592ded60ca652e24d3f175fe37a26dca9b2b
januszpolak <janusz.polak@interia.pl>
2020-06-18 10:49:24 +0200

    add simple js validation form and add custom look to checkbox input

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,25 @@ var close = document.querySelector('.close');
 
 show.addEventListener('click', function(){
     popup.classList.add('visible')
+    show.style.display='none'; // hide CLICK ME! button to prevent show it in responsive view
 });
 
 close.addEventListener('click', function(){
     popup.classList.remove('visible')
+});
+
+
+var form = document.querySelector('.form');
+var email = document.querySelector('#email');
+var password = document.querySelector('#password');
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    if(email.value == '') {
+        alert('Please provide your email address')
+    }
+    if(password.value == '') {
+        alert('Please provide your password')
+    }
 });
