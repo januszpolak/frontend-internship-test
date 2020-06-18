9f66326919f8568172dc19476b9d3347dec7b976
januszpolak <janusz.polak@interia.pl>
2020-06-18 09:26:09 +0200

    add login form showing after click on CLICK ME button, apply styles and close function

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
 /* Here goes your JS code */
+
+var show = document.getElementById('show-popup-form');
+var popup = document.querySelector('.popup');
+var close = document.querySelector('.close');
+
+show.addEventListener('click', function(){
+    popup.classList.add('visible')
+});
+
+close.addEventListener('click', function(){
+    popup.classList.remove('visible')
+});
